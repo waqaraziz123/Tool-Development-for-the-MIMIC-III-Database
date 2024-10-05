8c1bdf6f6dd6f176cb533e7b5a1406077c308cb9
Waqar Aziz <34290579+waqaraziz123@users.noreply.github.com>
2024-10-06 01:20:32 +0500

    Update ids.sql

--- ids.sql
+++ ids.sql
@@ -1,1 +1,3 @@
+-- This code can generate the 'ids.csv' file used in mimic_numerics.py
+
 \copy (select icustay_id,subject_id from icustays order by icustay_id) to 'C:\Users\Desktop\ids.csv' CSV header;
